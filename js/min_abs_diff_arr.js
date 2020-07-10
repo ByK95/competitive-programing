0595ae43c36fd858078156a0f68445b281c9868a
dkdk225 <deniz225kaya@gmail.com>
2020-07-10 09:17:01 +0300

    min absolute difference in array solution

---
+++ js/min_abs_diff_arr.js
@@ -0,0 +1,55 @@
+// https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=greedy-algorithms
+// solved first try
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the minimumAbsoluteDifference function below.
+function minimumAbsoluteDifference(arr) {
+    arr = arr.sort(function(a,b){return a-b;});
+    let min;
+    for (let index = 0; index < arr.length - 1; index++) {
+        let diff = Math.abs(arr[index] - arr[index+1]);
+        if(min === undefined || diff < min){
+            min = diff;
+        }
+    }
+    return min;
+}
+
+function main() {
+    // const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = minimumAbsoluteDifference(arr);
+
+    // ws.write(result + '\n');
+    console.log(result + '\n');
+
+    // ws.end();
+}
