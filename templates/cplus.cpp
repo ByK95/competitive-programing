8798d090d884e48694723830db3d816047d755bf
ByK95 <ByK95@github.com>
2020-09-09 18:10:15 +0300

    update template

--- templates/cplus.cpp
+++ templates/cplus.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 #define REP(i,a,b) for (int i = a; i <= b; i++)
 #define RP(i,a,b) for (int i = a; i < b; i++)
+#define REN(i,b) for (int i = b - 1; i >= 0; i--)
+#define PV(x) cout << #x"=" << x << '\n'
 
 int main() {
     cin.tie(0);
