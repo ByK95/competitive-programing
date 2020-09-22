53a3a4cd937537354a318482b5c21057982bbd9a
ByK95 <ByK95@github.com>
2020-09-22 10:16:33 +0300

    add solution for structs

---
+++ cplus/structs/structs.cpp
@@ -0,0 +1,29 @@
+// 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define RP(i,a,b) for (int i = a; i < b; i++)
+#define REN(i,b) for (int i = b - 1; i >= 0; i--)
+#define PV(x) cout << #x"=" << x << '\n'
+
+struct Student
+{
+    int age;
+    string first_name;
+    string last_name;
+    int standard;
+};
+
+
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+    Student st;
+    
+    cin >> st.age >> st.first_name >> st.last_name >> st.standard;
+    cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
+    
+    return 0;
+}
