ae26787174e97b694bd3bbe7d49b8d71fed182b1
ByK95 <ByK95@github.com>
2020-10-04 11:01:02 +0300

    add solution to inherited code

---
+++ cplus/inherited-code/inherited-code.cpp
@@ -0,0 +1,57 @@
+// https://www.hackerrank.com/challenges/inherited-code/problem
+#include <bits/stdc++.h>
+#include <exception>
+
+using namespace std;
+
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define RP(i,a,b) for (int i = a; i < b; i++)
+#define REN(i,b) for (int i = b - 1; i >= 0; i--)
+#define PV(x) cout << #x"=" << x << '\n'
+
+struct BadLengthException : public exception
+{
+	int n;
+	BadLengthException(int v){
+		n = v;
+	}
+    virtual const char* what() const throw(){
+		static char buf[4];
+		sprintf(buf,"%d",n);
+        return buf;
+    }
+};
+
+
+bool checkUsername(string username) {
+	bool isValid = true;
+	int n = username.length();
+	if(n < 5) {
+		throw BadLengthException(n);
+	}
+	for(int i = 0; i < n-1; i++) {
+		if(username[i] == 'w' && username[i+1] == 'w') {
+			isValid = false;
+		}
+	}
+	return isValid;
+}
+
+int main() {
+	int T; cin >> T;
+	while(T--) {
+		string username;
+		cin >> username;
+		try {
+			bool isValid = checkUsername(username);
+			if(isValid) {
+				cout << "Valid" << '\n';
+			} else {
+				cout << "Invalid" << '\n';
+			}
+		} catch (BadLengthException e) {
+			cout << "Too short: " << e.what() << '\n';
+		}
+	}
+	return 0;
+}
